fa4c1f59a7bcb22bd07f9e22973d1e40e5fde99b
ageekymonk <ramzthecoder@gmail.com>
2017-12-24 09:16:59 +1100

    Moving all redux action,stores and reducers to redux folder

--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import App from './App';
-import AppConfig from './Fire';
-import {store} from './store/configureStore';
-import registerServiceWorker from './registerServiceWorker';
+import AppContainer from 'containers/AppContainer';
+import AppConfig from 'firebase/Fire';
+import {store} from 'redux/store/configureStore';
+import registerServiceWorker from 'registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-
-
+import 'styles/index.css';
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App appConfig={AppConfig}/>
+            <AppContainer appConfig={AppConfig}/>
         </BrowserRouter>
     </Provider>
+
     , document.getElementById('root'));
 
 registerServiceWorker();
--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
-import rootReducer from '../reducers';
-import * as ProjectSaga from '../sagas/ProjectSaga';
+import rootReducer from '../reducers/index';
+import * as ProjectSaga from '../../sagas/ProjectSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
